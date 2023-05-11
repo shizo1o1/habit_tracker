c3eccd6ce0fd3b80ba24376e43d120a2a5caf1f5
shizo1o1 <121438034+shizo1o1@users.noreply.github.com>
2023-05-12 01:31:48 +0600

    Implemented CRUD for Habit

--- src/main/java/com/example/habit_tracker/controllers/HabitController.java
+++ src/main/java/com/example/habit_tracker/controllers/HabitController.java
@@ -1,4 +1,87 @@
 package com.example.habit_tracker.controllers;
 
+import com.example.habit_tracker.models.Habit;
+import com.example.habit_tracker.repo.HabitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+@Controller
 public class HabitController {
+    @Autowired
+    private HabitRepository habitRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        Iterable<Habit> habits = habitRepository.findAll();
+        model.addAttribute("habit", habits);
+        return "index";
+    }
+
+    @GetMapping("/add-habit")
+    public String postAddHabit(Model model){
+        return "add-habit";
+    }
+
+    @PostMapping("/add-habit")
+    public String addHabit(@RequestParam String name,@RequestParam String description, @RequestParam String dateStart, @RequestParam String dateFinish, @RequestParam int target, @RequestParam int frequency){
+        Habit habit = new Habit(name, description, dateStart, dateFinish, target, frequency);
+        habitRepository.save(habit);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String detailsHabit(@PathVariable(value = "id") long id, Model model){
+        if (!habitRepository.existsById(id)){
+            return ("redirect:/");
+        }
+        Optional<Habit> habit =habitRepository.findById(id);
+        ArrayList<Habit> result =new ArrayList<>();
+        habit.ifPresent(result::add);
+        model.addAttribute("habit", result);
+        return "details-habit";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editHabit(@PathVariable(value = "id") long id, Model model){
+        if (!habitRepository.existsById(id)){
+            return ("redirect:/");
+        }
+        Optional <Habit> product = habitRepository.findById(id);
+        ArrayList<Habit> result = new ArrayList<>();
+        product.ifPresent(result::add);
+        model.addAttribute("habit", result);
+        return "edit-habit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String postEditHabit(@RequestParam String name, @RequestParam String description, @RequestParam int target,@RequestParam int frequency, @RequestParam String dateStart, @RequestParam String dateFinish, @PathVariable(value = "id") long id,  Model model){
+        if (!habitRepository.existsById(id)){
+            return ("redirect:/");
+        }
+        Habit habit = habitRepository.findById(id).orElseThrow();
+        habit.setName(name);
+        habit.setDescription(description);
+        habit.setTarget(target);
+        habit.setFrequency(frequency);
+        habit.setDateStart(dateStart);
+        habit.setDateFinish(dateFinish);
+        habitRepository.save(habit);
+        return "redirect:/";
+    }
+    @PostMapping("/{id}/remove")
+    public String removeProduct(@PathVariable(value = "id") long id){
+        Habit habit = habitRepository.findById(id).orElseThrow();
+        habitRepository.delete(habit);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/example/habit_tracker/models/Habit.java
+++ src/main/java/com/example/habit_tracker/models/Habit.java
@@ -11,11 +11,75 @@ import java.util.Calendar;
 public class Habit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String name, description;
-
+    private Long id;
+    private String name, description, dateStart, dateFinish;
     private int target,  frequency;
 
-    Calendar dateStart, dateFinish;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(String dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public String getDateFinish() {
+        return dateFinish;
+    }
+
+    public void setDateFinish(String dateFinish) {
+        this.dateFinish = dateFinish;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public Habit() {
+    }
+
+    public Habit(String name, String description, String dateStart, String dateFinish, int target, int frequency) {
+        this.name = name;
+        this.description = description;
+        this.dateStart = dateStart;
+        this.dateFinish = dateFinish;
+        this.target = target;
+        this.frequency = frequency;
+    }
 }
